1017cff5cbd7ac4cd15b80246bfb563253b9f089
fjcamillo <fjcamillo.dev@gmail.com>
2016-11-05 05:23:52 +0800

    added the function that connects different site pages

--- HeavensMelody/HeavensMelody/settings.py
+++ HeavensMelody/HeavensMelody/settings.py
@@ -37,9 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'landing',
-    'login',
-    'pushmodule',
+    # 'landing',
+    # 'login',
+    # 'pushmodule',
 ]
 
 MIDDLEWARE = [
